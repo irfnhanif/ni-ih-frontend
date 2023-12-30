affdb359165f4e34a3b30eff11fc81af3ca359a1
Irfan Hanif <14.irfanh@gmail.com>
2023-12-30 10:27:03 +0700

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,13 @@ import {
   Td,
   Th,
   Tr,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
 } from "@chakra-ui/react";
 
 import { useContext, useEffect, useState } from "react";
@@ -18,69 +25,149 @@ import Navbar from "../components/navbar";
 import { AuthContext } from "../utils/AuthContext";
 
 export default function Home() {
-  const [mahasiswas, setMahasiswas] = useState([]);
+  const [books, setBooks] = useState([]);
   const [user, setUser] = useState(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
+  const [bookToEdit, setBookToEdit] = useState(null);
   const { token, setToken } = useContext(AuthContext);
 
-  async function getAllMahasiswa() {
+  async function getAllBooks() {
     try {
-      const res = await backend.get(`/mahasiswa`);
+      const res = await backend.get(`/books`, {
+        headers: {
+          Accept: "application/json",
+          token,
+        },
+      });
       const data = res.data;
 
-      console.log(data.mahasiswa);
-      setMahasiswas(data.mahasiswa);
+      console.log(data);
+      setBooks(data);
     } catch (error) {
       console.log(error);
     }
   }
 
-  const hasUserLogedIn = async () => {
+  const getBookById = async (book_id) => {
     try {
-      const res = await backend.get('/mahasiswa/profile', {
+      const res = await backend.get(`/books/${book_id}`, {
         headers: {
+          Accept: "application/json",
           token,
-          validateStatus: false,
         },
-      })
+      });
 
       if (res.status !== 200) {
         alert(res.data.message);
         return;
       }
 
-      return setUser(res.data.mahasiswa);
+      const book = res.data.book;
+      return book;
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleLogout = () => {
-    setToken(null);
-    setUser(null);
-  }
-
-  const handleDelete = async (nim) => {
+  const hasUserLoggedIn = async () => {
     try {
-      const res = await backend.delete(`/mahasiswa/${nim}`, {
+      const res = await backend.get("/user", {
         headers: {
+          Accept: "application/json",
           token,
-          validateStatus: false,
+        },
+      });
+
+      if (res.status !== 200) {
+        alert(res.data.message);
+        return;
+      }
+
+      return setUser(res.data.user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const res = await backend.delete("/user/logout", {
+        headers: {
+          Accept: "application/json",
+          token,
+        },
+      });
+
+      if (res.status !== 200) {
+        alert(res.data.message);
+        return;
+      }
+
+      setToken(null);
+      setUser(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDetail = async (book_id) => {
+    const book = await getBookById(book_id);
+
+    setBookToEdit(book);
+    setIsEditModalOpen(true);
+  };
+
+  const handleEdit = async (book_id) => {
+    const book = await getBookById(book_id);
+
+    setBookToEdit(book);
+    setIsEditModalOpen(true);
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const res = await backend.put(`/books/${bookToEdit.id}`, JSON.stringify(bookToEdit), {
+        headers: {
+          Accept: "application/json",
+          token,
+        },
+      });
+
+      if (res.status !== 200) {
+        alert(res.data.message);
+        return;
+      }
+
+      alert(res.data.message);
+      setIsEditModalOpen(false);
+      getAllBooks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (book_id) => {
+    try {
+      const res = await backend.delete(`/books/${book_id}`, {
+        headers: {
+          Accept: "application/json",
+          token,
         },
       });
 
       const data = res.data;
       console.log(data);
-      getAllMahasiswa();
-      handleLogout();
-      alert('mahasiswa deleted');
+      getAllBooks();
+      alert(res.data.message);
     } catch (error) {
       alert(error.message);
     }
   };
 
   useEffect(() => {
-    hasUserLogedIn();
-    getAllMahasiswa();
+    hasUserLoggedIn();
+    getAllBooks();
   }, [token]);
 
   return (
@@ -105,22 +192,24 @@ export default function Home() {
             <Thead>
               <Tr>
                 <Th>No</Th>
-                <Th>NIM</Th>
-                <Th>Nama</Th>
-                <Th>Angkatan</Th>
+                <Th>ISBN</Th>
+                <Th>Title</Th>
+                <Th>Author</Th>
                 <Th>Action</Th>
               </Tr>
             </Thead>
             <Tbody>
-              {mahasiswas &&
-                mahasiswas.map((mahasiswa, index) => (
-                  <Tr key={mahasiswa.nim}>
+              {books &&
+                books.map((book, index) => (
+                  <Tr key={book.id}>
                     <Td>{index + 1}</Td>
-                    <Td>{mahasiswa.nim}</Td>
-                    <Td>{mahasiswa.nama}</Td>
-                    <Td>{mahasiswa.angkatan}</Td>
+                    <Td>{book.isbn}</Td>
+                    <Td>{book.title}</Td>
+                    <Td>{book.author}</Td>
                     <Td>
-                      <Button onClick={() => handleDelete(mahasiswa.nim)}>
+                      <Button onClick={() => handleDetail(book.id)}>Detail</Button>
+                      <Button onClick={() => handleEdit(book.id)}>Edit</Button>
+                      <Button onClick={() => handleDelete(book.id)}>
                         Delete
                       </Button>
                     </Td>
@@ -129,6 +218,143 @@ export default function Home() {
             </Tbody>
           </Table>
         </TableContainer>
+
+        <Modal
+          isOpen={isDetailModalOpen}
+          onClose={() => setIsDetailModalOpen(false)}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Detail Book</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                <p>ISBN: {bookToEdit?.isbn}</p>
+                <p>Title: {bookToEdit?.title}</p>
+                <p>Author: {bookToEdit?.author}</p>
+                <p>Published: {bookToEdit?.published}</p>
+                <p>Publisher: {bookToEdit?.publisher}</p>
+                <p>Pages: {bookToEdit?.pages}</p>
+                <p>Description: {bookToEdit?.description}</p>
+                <p>Website: {bookToEdit?.website}</p>
+            </ModalBody>
+            <ModalFooter>
+              <Button variant="ghost" onClick={() => setIsDetailModalOpen(false)}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        <Modal
+          isOpen={isEditModalOpen}
+          onClose={() => setIsEditModalOpen(false)}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Edit Book</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <FormControl id="isbn">
+                <FormLabel>ISBN</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.isbn || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, isbn: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="title">
+                <FormLabel>Title</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.title || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, title: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="author">
+                <FormLabel>Author</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.author || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, author: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="published">
+                <FormLabel>Published</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.published || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, published: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="publisher">
+                <FormLabel>Publisher</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.publisher || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, publisher: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="pages">
+                <FormLabel>Pages</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.pages || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, pages: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="description">
+                <FormLabel>Description</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.description || ""}
+                  onChange={(e) =>
+                    setBookToEdit({
+                      ...bookToEdit,
+                      description: e.target.value,
+                    })
+                  }
+                />
+              </FormControl>
+
+              <FormControl id="website">
+                <FormLabel>Website</FormLabel>
+                <Input
+                  type="text"
+                  value={bookToEdit?.website || ""}
+                  onChange={(e) =>
+                    setBookToEdit({ ...bookToEdit, website: e.target.value })
+                  }
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={() => handleUpdate()}>
+                Save
+              </Button>
+              <Button variant="ghost" onClick={() => setIsEditModalOpen(false)}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
     </Box>
   );
